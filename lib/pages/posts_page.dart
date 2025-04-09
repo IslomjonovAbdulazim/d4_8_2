2bf26667e90374219562888de188bd873e36bb75
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-09 18:26:08 +0500

    imp

--- lib/pages/posts_page.dart
+++ lib/pages/posts_page.dart
@@ -1,5 +1,10 @@
-import '../models/post_model.dart';
+import 'package:d4_8_2/pages/comments_page.dart';
+import 'package:d4_8_2/service/network_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../models/post_model.dart';
 
 class PostsPage extends StatefulWidget {
   const PostsPage({super.key});
@@ -10,6 +15,7 @@ class PostsPage extends StatefulWidget {
 
 class _PostsPageState extends State<PostsPage> {
   List<PostModel> posts = [];
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -17,10 +23,59 @@ class _PostsPageState extends State<PostsPage> {
     super.initState();
   }
 
-  void load() async {}
+  void load() async {
+    isLoading = true;
+    setState(() {});
+    posts = await NetworkService().getAllPosts();
+    isLoading = false;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: SafeArea(
+        child: isLoading
+            ? Center(child: CircularProgressIndicator.adaptive())
+            : ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  final model = posts[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 6),
+                    child: CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      color: Colors.indigo,
+                      onPressed: () {
+                        Get.to(CommentsPage(postID: model.id));
+                      },
+                      child: ListTile(
+                        title: Text(
+                          model.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        subtitle: Text(
+                          model.body,
+                          style: TextStyle(
+                            color: Colors.white,
+                            height: 1,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
   }
 }
