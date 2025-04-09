4be6830f89d298d2c7c5d8f401ae57e9b460b2b7
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-09 18:41:08 +0500

    imp

--- lib/pages/comments_page.dart
+++ lib/pages/comments_page.dart
@@ -1,7 +1,10 @@
+import 'package:d4_8_2/models/comment_model.dart';
+import 'package:d4_8_2/service/network_service.dart';
 import 'package:flutter/material.dart';
 
 class CommentsPage extends StatefulWidget {
   final int postID;
+
   const CommentsPage({super.key, required this.postID});
 
   @override
@@ -9,14 +12,49 @@ class CommentsPage extends StatefulWidget {
 }
 
 class _CommentsPageState extends State<CommentsPage> {
+  bool isLoading = false;
+  List<CommentModel> comments = [];
+
   @override
   void initState() {
+    load();
     super.initState();
+  }
+
+  void load() async {
+    isLoading = true;
+    setState(() {});
+    comments = await NetworkService().getAllComments(widget.postID);
+    isLoading = false;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Comments"),
+      ),
+      body: SafeArea(
+        child: isLoading
+            ? Center(child: CircularProgressIndicator.adaptive())
+            : ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                itemCount: comments.length,
+                itemBuilder: (context, index) {
+                  final model = comments[index];
+                  return Padding(
+                    padding: EdgeInsets.symmetric(vertical: 6),
+                    child: ListTile(
+                      tileColor: Colors.indigo,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
   }
 }
-
