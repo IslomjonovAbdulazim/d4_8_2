18a036d994fea9a566558da7f7288a2bbdb2f53c
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-09 17:48:29 +0500

    imp

--- lib/pages/albums_page.dart
+++ lib/pages/albums_page.dart
@@ -1,3 +1,5 @@
+import 'package:d4_8_2/models/albums_model.dart';
+import 'package:d4_8_2/service/network_service.dart';
 import 'package:flutter/material.dart';
 
 class AlbumsPage extends StatefulWidget {
@@ -8,14 +10,40 @@ class AlbumsPage extends StatefulWidget {
 }
 
 class _AlbumsPageState extends State<AlbumsPage> {
+  bool isLoading = false;
+  List<AlbumsModel> albums = [];
 
   @override
   void initState() {
+    load();
     super.initState();
+  }
+
+  void load() async {
+    isLoading = true;
+    setState(() {});
+    albums = await NetworkService().getAllAlbums();
+    isLoading = false;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: isLoading
+            ? Center(child: CircularProgressIndicator.adaptive())
+            : ListView.builder(
+                itemCount: albums.length,
+                itemBuilder: (context, index) {
+                  final model = albums[index];
+                  return ListTile(
+                    title: Text(model.title),
+                    subtitle: Text("ID: ${model.id}, User Id: ${model.userId}"),
+                  );
+                },
+              ),
+      ),
+    );
   }
 }
